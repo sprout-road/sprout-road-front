1a4779bb5c4ae55d88c0e83268cb47cb6a771b71
beomjo <dongjun1315@naver.com>
2025-07-30 20:25:35 +0900

    feat(component): 공통 에러 컴포넌트 생성

---
+++ src/components/common/Error.tsx
@@ -0,0 +1,16 @@
+interface ErrorComponentProps {
+    error: string
+    refetch: () => void
+}
+
+function ErrorComponent({error, refetch}: ErrorComponentProps) {
+    return (
+        <div className="flex justify-center text-red-500">
+            <p>데이터를 불러오는데 실패했습니다.</p>
+            <p>{error}</p>
+            <button onClick={refetch} className="bg-blue-400 text-white font-bold"/>
+        </div>
+    )
+}
+
+export default ErrorComponent
