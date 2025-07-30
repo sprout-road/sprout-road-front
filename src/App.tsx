e7be15bf7b2d76ba84872156a62c3a6aa463a6e8
jihwankim128 <jihwan1535@nate.com>
2025-07-31 05:51:26 +0900

    feat: 트래블 로그 기능 구현만

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from './pages/home';
 import Mission from './pages/mission'
 import RegionColoring from './pages/RegionColoring';
 import RegionDetail from './pages/RegionDetails';
+import TravelLogEditor from './pages/TravelLogEditor';
 import './index.css';
 
 function App() {
@@ -14,9 +15,10 @@ function App() {
                 <Route path="/mission" element={<Mission />} />
                 <Route path="/region-coloring" element={<RegionColoring />} />
                 <Route path="/region/:sidoCode" element={<RegionDetail />} />
+                <Route path="/travel-log/new" element={<TravelLogEditor />} />
             </Routes>
         </LocationProvider>
-    ); 
+    );
 }
 
 export default App;
